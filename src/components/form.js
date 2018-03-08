0e668fc2dd35e1cfd4dbcec5b97118ae0b83a95b
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2018-03-08 08:23:22 -0700

    WIP- Stateful components for game

--- src/components/form.js
+++ src/components/form.js
@@ -2,42 +2,97 @@ import React from 'react'
 
 import './form.css';
 
-export default function Form(props) {
-  let userGuess, message;
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const computerGuess = Math.floor(Math.random() * 100);
-  console.log(computerGuess);
+    this.state = {
+      userGuess: 0,
+      computerGuess: Math.floor(Math.random() * 100),
+      result: 0
+    }
 
-  const giveClue = (userGuess, computerGuess) => {
-    let result = ((computerGuess - userGuess) / computerGuess) * 100;
-    console.log('result is', computerGuess);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  setUserGuess(userGuess) {
+    this.setState({
+      userGuess
+    })
+  }
+
+  setComputerGuess(computerGuess) {
+    this.setState({
+      computerGuess
+    })
+  }
+
+  handleChange(event) {
+    // event.preventDefault();
+    this.setState({userGuess: event.target.value});
+    console.log('userguess is' + this.state.userGuess);
+  }
+
+  handleSubmit(event) {
+    // alert('Guess was submitted: ' + this.state.userGuess);
+    event.preventDefault();
+  }
+
+  giveClue(result) {
+    console.log('Form was submitted');
+    let result = ((this.state.computerGuess - this.state.userGuess) / this.state.computerGuess) * 100;
     if (result <= 10) {
-      alert('You are hot');
+      alert('You are hot')
     } else {
-      alert('You are cold');
+      alert('You are cold')
     }
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("user submission", userGuess.value)
+  render() {
+    return (
+       <form onSubmit={this.handleSubmit}>
+        <h1>Guess the Number</h1>
+           <div>
+             <label htmlFor="userGuess">Guess:</label>
+             <input
+               type="number"
+               id="userGuess"
+               min={0}
+               max={100}
+               value={this.state.userGuess}
+               onChange={this.handleChange}
+             />
+           </div>
+           <div className="button">
+             <button type="submit" onClick={this.giveClue(this.state.result)}>Submit</button>
+           </div>
+       </form>
+     );
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h1>Guess the Number</h1>
-        <div>
-          <label htmlFor="userGuess">Guess:</label>
-          <input type="number"
-            id="userGuess"
-            min={0}
-            max={100}
-            ref={guess => userGuess = guess}
-          />
-        </div>
-        <div className="button">
-          <button type="submit" onClick={giveClue}>Submit</button>
-        </div>
-    </form>
-  );
 }
+
+// default function Form(props) {
+//   let userGuess, message;
+//
+//   const computerGuess = Math.floor(Math.random() * 100);
+//   console.log(computerGuess);
+//
+//   const giveClue = (userGuess, computerGuess) => {
+//     let result = ((computerGuess - userGuess) / computerGuess) * 100;
+//     console.log('result is', computerGuess);
+//     if (result <= 10) {
+//       alert('You are hot');
+//     } else {
+//       alert('You are cold');
+//     }
+//   }
+//
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log("user submission", userGuess.value)
+//   }
+//
+//
+// }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/form'
 import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
 
 ReactDOM.render(
   <Form />,
   document.getElementById('root')
 );
-// registerServiceWorker();
